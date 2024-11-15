f18d4166f87eac67839dd213015002f6215f7e25
lash0000 <k80308392@gmail.com>
2024-11-15 11:37:22 +0800

    Refactor: BPSO UI :angry: @Aeroxism

---
+++ views/dashboard/departments/BPSO/sidebarRoutes.js
@@ -0,0 +1,16 @@
+// Add new Side Menu here
+export const sidebarRoutes = [
+
+    // BPSO Things
+
+    { className: "main", path: "/views/dashboard/departments/BPSO/Dashboard/dashboard.php" },
+    { className: "complaint-section", path: "/views/dashboard/departments/BPSO/Complaint%20main/complaints.php" },
+    { className: "new-complaint", path: "/views/dashboard/departments/BPSO/Complaint%20main/newcomplaint.php" },
+    { className: "create-report", path: "/views/dashboard/departments/BPSO/Complaint%20main/report.php" },
+    { className: "team-schedule", path: "/views/dashboard/departments/BPSO/Team%20Schedule/teamschedule.php" },
+    { className: "vehicle-dispatch", path: "/views/dashboard/departments/BPSO/Vehicle%20Dispatchment/vehicle.php"},
+
+    // LUPON Things
+
+
+];
