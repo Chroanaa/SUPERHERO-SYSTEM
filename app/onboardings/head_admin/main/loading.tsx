f43be6ff90d154f9cc454aa6dab1d73fc93c8c14
lash0000 <k80308392@gmail.com>
2025-07-07 11:20:41 +0800

    cleanup now wip :fire:

--- app/onboardings/head_admin/main/loading.tsx
+++
@@ -1,9 +0,0 @@
-import { Progress } from "@/components/ui/progress"
-
-export default function Loading() {
-    return (
-        <div className="flex justify-center items-center h-screen">
-            <Progress value={88} className="w-[60%]" />
-        </div>
-    )
-}
